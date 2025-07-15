1f9347aa2e1ae9c15e1b39d3c7a4514b56d9731e
Emerson Albornoz <emerson02325@gmail.com>
2025-07-15 15:39:06 -0500

    Task y Heap logic

--- src/Modelo/Heap.java
+++ src/Modelo/Heap.java
@@ -9,5 +9,40 @@ public class Heap {
         heaps = new ArrayList<>();
     }
 
+    public void insert(Task task) {
+        heaps.add(task);
+        heapifyUp(heaps.size() - 1);
+
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heaps.get(index).priority < heaps.get(parent).priority) {
+                exchange(index, parent);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void exchange(int index1, int index2) {
+        Task temp = heaps.get(index1);
+        heaps.set(index1, heaps.get(index2));
+        heaps.set(index2, temp);
+    }
+
+    private void heapifyDown(int index) {
+        int size = heaps.size();
+
+        while (true) {
+            int left = index * 2 + 1;
+            int right = left + 1;
+            int lowest = index;
+
+        }
+    }
+
 
 }
