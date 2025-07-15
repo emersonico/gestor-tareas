5b99ea86d7a13d6c66db292b58b6fa80918889ee
Emerson Albornoz <emerson02325@gmail.com>
2025-07-15 12:32:34 -0500

    Creación de la clase tarea

--- src/Modelo/Tarea.java
+++ src/Modelo/Tarea.java
@@ -2,4 +2,19 @@ package Modelo;
 
 public class Tarea {
     int id; // el identificados unico
+    int prioridad;
+    String descripcion;
+
+    public Tarea(int id, int prioridad, String descripcion) {
+        this.id = id;
+        this.prioridad = prioridad;
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" + "id=" + id + ", prioridad=" + prioridad + ", descripcion='" + descripcion + '\'' + '}';
+    }
+
+
 }
