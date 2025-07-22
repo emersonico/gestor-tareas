a7224095d3b00e854f5792c2731b5e40c2c5b79e
Emerson Albornoz <emerson02325@gmail.com>
2025-07-22 10:55:20 -0500

    Test: CLI version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import Modelo.Task;
+import Modelo.System;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System sistema = new System();
+
+        sistema.addTask(new Task(1,5, "Informe final"));
+        sistema.addTask(new Task(2,2, "Revisar codigo"));
+        sistema.addTask(new Task(3,7, "Presentación"));
+        sistema.addTask(new Task(4,1, "Reunión urgente"));
+
+        sistema.processTask();
+        sistema.processTask();
     }
 }
